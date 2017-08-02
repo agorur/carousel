18d15d68c9a832e66e547523a6990ef149f2379e
Ajay Gorur <Ajay.Gorur@aexp.com>
2017-08-02 11:40:51 -0700

    added navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Ng2CarouselamosModule } from 'ng2-carouselamos';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AnimalsComponent } from './animals/animals.component';
+import { WaterfallsComponent } from './waterfalls/waterfalls.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimalsComponent,
+    WaterfallsComponent
   ],
   imports: [
     BrowserModule,
     Ng2CarouselamosModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'animal',
+        component: AnimalsComponent
+      },
+      {
+        path: 'waterfall',
+        component: WaterfallsComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
